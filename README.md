4581fded1bdb37c7ff74b636434f4a870ae85d8b
wyvern <wyvern@uab.edu>
2019-08-28 12:33:54 -0500

    More stories, formatting fixes, added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Story Generator
+
+A simple but fun console program that creates a randomly selected story based on user input.  
+There are 5 possible story templates, and the user has no idea which will be chosen when he is entering the different values.  
+This program is modified from one I wrote for fun when I was in my first programming course. Now the code looks a good bit nicer, and the added randomization makes it a lot more fun.
--- StoryGenerator.cpp
+++ StoryGenerator.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 class StoryMaker
@@ -13,22 +14,22 @@ public:
 	// prompts user to fill in variables
 	void getInput()
 	{
-		cout << "Type in a food, and then press Enter.\n";
+		cout << "Enter a food.\n>";
 		food = getAns(food);
-		cout << "Type in a creature, then press Enter.\n";
+		cout << "Enter a creature (real or mythical).\n>";
 		creature = getAns(creature);
-		cout << "Enter the name of any item, like a lamp or a tree.\n";
+		cout << "Enter an item (e.g. lamp, tree).\n>";
 		item = getAns(item);
-		cout << "Enter an interjection, capitalized but without punctuation.\n";
+		cout << "Enter an interjection, without punctuation.\n>";
 		interjection = getAns(interjection);
-		cout << "Enter a name, either normal or kooky.\n";
+		cout << "Enter a name.\n>";
 		name = getAns(name);
-		cout << "Enter a place.\n";
+		cout << "Enter a place.\n>";
 		place = getAns(place);
-		cout << "Enter an emotion (as in, I am feeling ___).\n";
+		cout << "Enter an emotion (as in, I am feeling ___).\n>";
 		emotion = getAns(emotion);
-		cout << "Enter a utensil or tool.\n";
-		utensil = getAns(utensil);
+		cout << "Enter a utensil or tool.\n>";
+		tool = getAns(tool);
 		cout << endl;
 	}
 
@@ -36,12 +37,38 @@ public:
 	// 	input values
 	string randomStory()
 	{
+		//be sure name and interjection are capitalized
+		name[0] = toupper(name[0]); 
+		interjection[0] = toupper(interjection[0]);
+
 		//random number to determine story
-		int randnum = rand() % 3 + 1;
-		string story;
+		int randnum = rand() % 5 + 1;
+		string story; 
 
 		switch (randnum)
 		{
+		case 5:
+			story = "If you're going to " + name + "'s Diner,\n"
+				+ "here's some things to know: \n"
+				+ "1. The roast " + creature + " doesn't come with a " + tool + ".\n"
+				+ "\tYou have to bring your own.\n"
+				+ "2. The " + food + " is so good, you'll say \"" + interjection + "!\"\n"
+				+ "3. The " + place + " special now comes with a free " + item + "\n"
+				+ "4. Everyone who goes to " + name + "'s comes out feeling " + emotion + "\n"
+				+ "Try " + name + "'s today!\n";
+			break;
+		case 4:
+			item[0] = toupper(item[0]);
+			story = "There once was a girl named " + name + ", but her friends\n" 
+				+ "called her " + item + " (it's a long story). Well one day " + item + "\n"
+				+ "was walking by the woods when she saw a " + creature + ". She was so\n"
+				+ emotion + " to see it there that she yelled out \"" + interjection + "!\n"
+				+ "\"A " + creature + "!\". She tried to convince it to come out by giving\n"
+				+ "it " + food + ". She even made it a little house out of a " + tool + " she\n"
+				+ "happened to have with her. But the " + creature + " went back in the woods.\n"
+				+ "\"Oh well,\" " + item + " said, \"I guess it's better off in " + place + "\n"
+				+ "in its natural habitat.\"\n";
+			break;
 		case 3:
 			story = "There once was a guy named " + name + ".\n"
 				+ "He liked " + food + ". One day, " + name + " was taking a walk\n"
@@ -49,12 +76,12 @@ public:
 				+ "He watched as it was walking by, until it turned around and said\n\""
 				+ interjection + "! What, you ain't never seen a " + creature + " before?\"\n"
 				+ "It tossed a " + item + " at him in disgust and left. " + name + "\n"
-				+ "was left feeling " + emotion + " and quietly went back to using his " + utensil + ".\n";
+				+ "was left feeling " + emotion + " and quietly went back to using his " + tool + ".\n";
 			break;
 		case 2:
 			story = "To become a successful " + creature + " herder,\n"
 				+ "you have to be like the great " + name + " of " + place + ":\n"
-				+ "he fed them plenty of " + food + ", he was skillful with a " + utensil + ",\n"
+				+ "he fed them plenty of " + food + ", he was skillful with a " + tool + ",\n"
 				+ "and he was always a " + emotion + " person who wasn't afraid to say \n"
 				+ "\"" + interjection + ", get back here you " + creature + "!\"\n"
 				+ "If you treat your " + creature + "s right just like " + name + ", they\n"
@@ -71,7 +98,7 @@ public:
 				+ name + " was " + emotion + ". \"What?! A " + food + " stand that only sells\n"
 				+ item + "s? That's ridiculous! I'm going to the stand in " + place + ".\n"
 				+ "They'll have better service.\" As he walked away, the " + creature + "\n"
-				+ "said \"Hey, wait! I could sell you a " + utensil + " instead!\"\n";
+				+ "said \"Hey, wait! I could sell you a " + tool + " instead!\"\n";
 			break;
 		}
 		return story;
@@ -79,14 +106,14 @@ public:
 
 private:
 	string food, creature, item, interjection,
-		name, place, emotion, utensil; 
+		name, place, emotion, tool; 
 
 	string getAns(string ans)
 	{
 		do 
 		{
 			getline(cin, ans);
-		} while (ans.length() < 2); //TODO: modify so that single-char answers are allowed
+		} while (ans.length() < 1);
 	return ans;
 	}
 };
@@ -102,7 +129,9 @@ int main()
 	do
 	{
 		clearScreen();
-		cout << "*******Story Generator*******\n";
+		cout << "\t*******************\n"
+			<< "\t| Story Generator |\n"
+			<< "\t*******************\n";
 		storymaker.getInput();
 		cout << "Okay, here's your story:\n\n";
 		cout << storymaker.randomStory();
